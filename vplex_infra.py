f39730d2f2d3390bebf10c8f526632595964ea78
Shounak Ray <shounak@stanford.edu>
2025-06-01 17:00:42 -0700

    custom topo test

--- vplex_infra.py
+++ vplex_infra.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python
-from mininet.topo import Topo, SingleSwitchTopo
+from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.log import lg, info
-from mininet.cli import CLI
 from mininet.node import OVSController
+from mininet.cli import CLI
+
+class DelayedTopo(Topo):
+    def build(self):
+        # Create two hosts and one switch
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        s1 = self.addSwitch('s1')
+        
+        # Add links with delay
+        self.addLink(h1, s1, cls=TCLink, delay='100ms')
+        self.addLink(h2, s1, cls=TCLink, delay='100ms')
 
 def main():
     lg.setLogLevel('info')
-    # Use OVS controller
-    net = Mininet(SingleSwitchTopo(k=2), controller=OVSController)
+    
+    # Instantiate the topology and pass TCLink to enable link options
+    topo = DelayedTopo()
+    net = Mininet(topo=topo, controller=OVSController, link=TCLink)
     net.start()
-    
+
     h1 = net.get('h1')
-    p1 = h1.popen('python myServer.py -i %s &' % h1.IP())
-    
     h2 = net.get('h2')
+
+    # Start server on h1
+    p1 = h1.popen('python myServer.py -i %s' % h1.IP())
+
+    # Run client on h2
     h2.cmd('python myClient.py -i %s -m "hello world"' % h1.IP())
-    
-    # CLI(net)
+
+    # Clean up
     p1.terminate()
     net.stop()
 
